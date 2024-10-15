4ef3035e2a543f6b41141cc4de152ef3de1ce16e
DFBlok <s219392862@mandela.ac.za>
2024-10-15 12:02:51 +0200

    update 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["landing-page-sigma-sage.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
