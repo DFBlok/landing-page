b2d352d54cd3c3421de038bb3ca9caead794beba
DFBlok <s219392862@mandela.ac.za>
2024-10-15 12:38:55 +0200

    price planning

--- components/PricePlan/PricePlan.tsx
+++ components/PricePlan/PricePlan.tsx
@@ -7,7 +7,7 @@ const PricePlan = () => {
         <div className="divide-y divide-gray-200 rounded-2xl border border-gray-200 shadow-sm">
           <div className="p-6 sm:px-8">
             <h2 className="text-lg font-medium text-gray-900">
-              Starter
+              Beginner
               <span className="sr-only">Plan</span>
             </h2>
 
@@ -18,10 +18,10 @@ const PricePlan = () => {
             <p className="mt-2 sm:mt-4">
               <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
                 {" "}
-                20${" "}
+                R2,875.00{" "}
               </strong>
 
-              <span className="text-sm font-medium text-gray-700">/month</span>
+              {/* <span className="text-sm font-medium text-gray-700">/month</span> */}
             </p>
 
             <a
@@ -158,6 +158,138 @@ const PricePlan = () => {
         <div className="divide-y divide-gray-200 rounded-2xl border border-gray-200 shadow-sm">
           <div className="p-6 sm:px-8">
             <h2 className="text-lg font-medium text-gray-900">
+              Intermediate
+              <span className="sr-only">Plan</span>
+            </h2>
+
+            <p className="mt-2 text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+
+            <p className="mt-2 sm:mt-4">
+              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                {" "}
+                R1725.00{" "}
+              </strong>
+
+              {/* <span className="text-sm font-medium text-gray-700">/month</span> */}
+            </p>
+
+            <a
+              className="mt-4 block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 sm:mt-6"
+              href="#"
+            >
+              Get Started
+            </a>
+          </div>
+
+          <div className="p-6 sm:px-8">
+            <p className="text-lg font-medium text-gray-900 sm:text-xl">
+              What&apos;s included:
+            </p>
+
+            <ul className="mt-2 space-y-2 sm:mt-4">
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+
+                <span className="text-gray-700"> 20 users </span>
+              </li>
+
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+
+                <span className="text-gray-700"> 5GB of storage </span>
+              </li>
+
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+
+                <span className="text-gray-700"> Phone support </span>
+              </li>
+
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+
+                <span className="text-gray-700"> Help center access </span>
+              </li>
+
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+
+                <span className="text-gray-700"> Community access </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* //tier 3 */}
+        <div className="divide-y divide-gray-200 rounded-2xl border border-gray-200 shadow-sm">
+          <div className="p-6 sm:px-8">
+            <h2 className="text-lg font-medium text-gray-900">
               Pro
               <span className="sr-only">Plan</span>
             </h2>
@@ -169,10 +301,10 @@ const PricePlan = () => {
             <p className="mt-2 sm:mt-4">
               <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
                 {" "}
-                30${" "}
+                R1800.00{" "}
               </strong>
 
-              <span className="text-sm font-medium text-gray-700">/month</span>
+              {/* <span className="text-sm font-medium text-gray-700">/month</span> */}
             </p>
 
             <a
